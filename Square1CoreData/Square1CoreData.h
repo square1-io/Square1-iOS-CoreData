db59edc25dc69706262b6087e0eb6dfe14ab3604
Roberto Pastor <wedge.hero@gmail.com>
2018-01-11 17:18:32 +0000

    Initial Commit

---
+++ Square1CoreData/Square1CoreData.h
@@ -0,0 +1,19 @@
+//
+//  Square1CoreData.h
+//  Square1CoreData
+//
+//  Created by Roberto Pastor Ortiz on 11/1/18.
+//  Copyright © 2018 Square1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Square1CoreData.
+FOUNDATION_EXPORT double Square1CoreDataVersionNumber;
+
+//! Project version string for Square1CoreData.
+FOUNDATION_EXPORT const unsigned char Square1CoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Square1CoreData/PublicHeader.h>
+
+
