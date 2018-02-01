d6728caaadd3c76c091575b2184b3d812daf7568
Roberto Prato <roberto.prato@gmail.com>
2018-02-01 16:31:00 +0000

    fixed issue with entity name when calling from parent class

--- Source/Protocols/Managed.swift
+++ Source/Protocols/Managed.swift
@@ -21,76 +21,76 @@
 import CoreData
 
 public protocol Managed: class, NSFetchRequestResult {
-  static var entityName: String { get }
-  static var defaultSortDescriptors: [NSSortDescriptor] { get }
+    static var entityName: String { get }
+    static var defaultSortDescriptors: [NSSortDescriptor] { get }
 }
 
 public extension Managed {
-  static var defaultSortDescriptors: [NSSortDescriptor] { return [] }
-  
-  static var sortedFetchRequest: NSFetchRequest<Self> {
-    let request = NSFetchRequest<Self>(entityName: entityName)
-    request.sortDescriptors = defaultSortDescriptors
-    return request
-  }
-  
-  public static func sortedFetchRequest(withPredicate predicate: NSPredicate) -> NSFetchRequest<Self> {
-    let request = sortedFetchRequest
-    request.predicate = predicate
-    return request
-  }
+    static var defaultSortDescriptors: [NSSortDescriptor] { return [] }
+    
+    static var sortedFetchRequest: NSFetchRequest<Self> {
+        let request = NSFetchRequest<Self>(entityName: entityName)
+        request.sortDescriptors = defaultSortDescriptors
+        return request
+    }
+    
+    public static func sortedFetchRequest(withPredicate predicate: NSPredicate) -> NSFetchRequest<Self> {
+        let request = sortedFetchRequest
+        request.predicate = predicate
+        return request
+    }
 }
 
 public extension Managed where Self: NSManagedObject {
-  static var entityName: String { return String(describing: self)  }
-  
-  static func insertObject(in context: NSManagedObjectContext) -> Self {
-    guard let obj = NSEntityDescription.insertNewObject(forEntityName: entityName, into: context) as? Self else { fatalError("Wrong object type") }
-    return obj
-  }
-  
-  static func count(in context: NSManagedObjectContext, matching predicate: NSPredicate? = nil) -> Int {
-    let request = NSFetchRequest<Self>(entityName: entityName)
-    request.predicate = predicate
-    return try! context.count(for: request)
-  }
-  
-  static func fetch(in context: NSManagedObjectContext,
-                    configurationBlock: ((NSFetchRequest<Self>) -> ())? = nil) -> [Self] {
-    let request = NSFetchRequest<Self>(entityName: Self.entityName)
-    configurationBlock?(request)
-    return try! context.fetch(request)
-  }
-  
-  static func findOrCreate(in context: NSManagedObjectContext,
-                           matching predicate: NSPredicate,
-                           configure: ((Self) -> ())? = nil) -> Self {
-    guard let object = findOrFetch(in: context, matching: predicate) else {
-      let newObject = Self.insertObject(in: context)
-      configure?(newObject)
-      return newObject
+    
+    static var entityName: String {
+        return String(describing: self)
     }
-    return object
-  }
-  
-  static func findOrFetch(in context: NSManagedObjectContext, matching predicate: NSPredicate) -> Self? {
-    guard let object = materializedObject(in: context, matching: predicate) else {
-      return fetch(in: context) { request in
+    
+    static func insertObject(in context: NSManagedObjectContext) -> Self {
+        guard let obj = NSEntityDescription.insertNewObject(forEntityName: entityName, into: context) as? Self else { fatalError("Wrong object type") }
+        return obj
+    }
+    
+    static func count(in context: NSManagedObjectContext, matching predicate: NSPredicate? = nil) -> Int {
+        let request = NSFetchRequest<Self>(entityName: entityName)
         request.predicate = predicate
-        request.returnsObjectsAsFaults = false
-        request.fetchLimit = 1
-        }.first
+        return try! context.count(for: request)
     }
-    return object
-  }
-  
-  static func materializedObject(in context: NSManagedObjectContext, matching predicate: NSPredicate) -> Self? {
-    for object in context.registeredObjects where !object.isFault {
-      guard let result = object as? Self, predicate.evaluate(with: result) else { continue }
-      return result
+    
+    static func fetch(in context: NSManagedObjectContext, configurationBlock: (NSFetchRequest<Self>) -> () = { _ in }) -> [Self] {
+        let request = NSFetchRequest<Self>(entityName: entityName)
+        configurationBlock(request)
+        return try! context.fetch(request)
     }
-    return nil
-  }
+    
+    static func findOrCreate(in context: NSManagedObjectContext, matching predicate: NSPredicate, configure: (Self) -> ()) -> Self {
+        guard let object = findOrFetch(in: context, matching: predicate) else {
+            let newObject = insertObject(in: context)
+            configure(newObject)
+            return newObject
+        }
+        return object
+    }
+    
+    static func findOrFetch(in context: NSManagedObjectContext, matching predicate: NSPredicate) -> Self? {
+        guard let object = materializedObject(in: context, matching: predicate) else {
+            return fetch(in: context) { request in
+                request.predicate = predicate
+                request.returnsObjectsAsFaults = false
+                request.fetchLimit = 1
+                }.first
+        }
+        return object
+    }
+    
+    static func materializedObject(in context: NSManagedObjectContext, matching predicate: NSPredicate) -> Self? {
+        for object in context.registeredObjects where !object.isFault {
+            guard let result = object as? Self, predicate.evaluate(with: result) else { continue }
+            return result
+        }
+        return nil
+    }
 }
 
 
